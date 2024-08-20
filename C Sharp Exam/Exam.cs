ff235a4c9501331cdf4fbcd5ae5476994e8b8b44
Ahmed Maher <ahmedsayed_g@sci.asu.edu.eg>
2024-08-20 12:20:45 +0300

    Add project files.

---
+++ C Sharp Exam/Exam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Exam
+{
+    internal abstract class Exam
+    {
+        public int Time { get; set; }
+        public int NumberOfQuestions { get; set; }
+        public Question[] ListOfQuestions { get; set; }
+
+    
+        public Exam(int time, int numberOfQuestions)
+        {
+            Time = time;
+            NumberOfQuestions = numberOfQuestions;
+        }
+     
+        public abstract void ShowExam();
+        public abstract void CreateListOfQuestions();
+    
+    }
+}
---
+++ C Sharp Exam/Question.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Exam
+{
+    internal abstract class Question
+    {
+        public abstract string Header { get; }
+        public string Body { get; set; }
+        public int Marks { get; set; }
+
+     
+        public Answer[] AnswerList { get; set; }
+        public Answer RightAnswer { get; set; }
+        public Answer UserAnswer { get; set; }
+       
+        public Question()
+        {
+            RightAnswer = new Answer();
+            UserAnswer = new Answer();
+        }
+     
+        public abstract void AddQuestion();
+        public override string ToString()
+        {
+            return $"{Header} \t Mark({Marks})\n" +
+                "-------------------------------------" +
+                $" \n{Body}\n";
+        }
+    }
+}
